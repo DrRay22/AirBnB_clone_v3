80bbc97217ed0000b32ed7deff3a8bb5f9fb2bb4
Ray Oshafi <scengltd@gmail.com>
2023-07-28 01:39:29 -0700

    Tasks Updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that imports a Blueprint and runs Flask """
+""" Script that import a Blueprint and runs Flask """
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 from models import storage
